59416c5d225aa19e1b8b5d35cbca22ecab7fb70a
andreaS <andrea.scorcia@superkoders.com>
2022-06-30 15:36:00 +0200

    Handle possible error when cloning repo

--- operations/cloneNextStarterKitRepo.go
+++ operations/cloneNextStarterKitRepo.go
@@ -1,6 +1,7 @@
 package operations
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 
@@ -8,9 +9,11 @@ import (
 )
 
 func CloneNextStarterKitRepo(projectName string) {
-	cloneRepoCommand := exec.Command("git", "clone", "git@bitbucket.org:superkoders/nextjs_stack.git", projectName)
-	cloneRepoCommand.Stdout = os.Stdout
-	cloneRepoCommand.Stderr = os.Stderr
-	cloneRepoCommand.Run()
+	cloneRepoCommand, err := exec.Command("git", "clone", "git@bitbucket.org:superkoders/nextjs_stack.git", projectName).Output()
+	if err != nil {
+		utils.Alert(fmt.Sprintf("Error cloning the repo. Check if a folder with that name already exists: %s", err) )
+		os.Exit(0)
+	}
+	utils.Success(string(cloneRepoCommand))
 	utils.Success("\n✔ Cloned SuperKoders' base Nextjs stack")
 }
